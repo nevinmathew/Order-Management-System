5f2c8c94f593225d1a39143c9588364da7aefe57
Nevin Mathew <88356497+nevinmathew@users.noreply.github.com>
2023-01-25 14:23:30 +0530

    Update OrderManagementApplication.java

--- OrderManagement/src/main/java/com/spring/orderMgmnt/OrderManagementApplication.java
+++ OrderManagement/src/main/java/com/spring/orderMgmnt/OrderManagementApplication.java
@@ -6,10 +6,29 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class OrderManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementApplication.class, args);
+	}
+	
+    	@Bean
+    	public Docket api() { 
+        	return new Docket(DocumentationType.SWAGGER_2)  
+          		.select()                                  
+          		.apis(RequestHandlerSelectors.any())              
+          		.paths(PathSelectors.any())                          
+          		.build();                                           
+    	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    		registry.addResourceHandler("swagger-ui.html")
+      		.addResourceLocations("classpath:/META-INF/resources/");
+
+    		registry.addResourceHandler("/webjars/**")
+      		.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
